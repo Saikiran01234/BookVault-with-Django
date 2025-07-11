4f78fe65bf9eeddd3d1d5d8ac293ac1b8605d639
Saikiran <josyulasai.03@gmail.com>
2025-07-11 11:50:02 +0530

    Second Commit

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -74,10 +75,10 @@ WSGI_APPLICATION = 'bookproject.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://bookvault_gb8r_user:uADFUkkVOf0DHON1YXbtENCVKGOO1Ta0@dpg-d1oan7nfte5s73b2of30-a.oregon-postgres.render.com/bookvault_gb8r',
+        conn_max_age=600
+    )
 }
 
 
