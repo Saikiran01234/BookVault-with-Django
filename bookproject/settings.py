14acd79a9bdb6a13e96ce85d7c86e936c7e64d79
Saikiran <josyulasai.03@gmail.com>
2025-07-11 12:47:37 +0530

    foruth commit

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -22,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wsn7#=1)@kw-u5*%f9g@pb+ped8$pki18+t!0=b57o=k55eo_b'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-wsn7#=1)@kw-u5*%f9g@pb+ped8$pki18+t!0=b57o=k55eo_b')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = []
+# Render provides the domain as an environment variable, or set manually
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
@@ -39,11 +40,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',  # Add whitenoise
     'bookapp',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add whitenoise middleware
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -80,7 +83,7 @@ WSGI_APPLICATION = 'bookproject.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://bookvault_gb8r_user:uADFUkkVOf0DHON1YXbtENCVKGOO1Ta0@dpg-d1oan7nfte5s73b2of30-a.oregon-postgres.render.com/bookvault_gb8r',
+        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
         conn_max_age=600
     )
 }
@@ -117,10 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'bookapp' / 'static',
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (Uploaded files)
 MEDIA_URL = '/media/'
